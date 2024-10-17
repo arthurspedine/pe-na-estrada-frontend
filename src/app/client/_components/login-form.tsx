194f4b7819ae64bd934ff32c4c8062bf53372d97
Vinicius de Oliveira Coutinho <viniciusoliveiracoutinho@gmail.com>
2024-10-17 10:54:26 -0300

    feat: login hook form
    feat: login hook form

---
+++ src/app/client/_components/login-form.tsx
@@ -0,0 +1,79 @@
+'use client'
+import { useForm } from 'react-hook-form'
+import { useRouter } from 'next/router'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+
+const loginSchema = z.object({
+  email: z
+    .string()
+    .email('Formato de email inválido.')
+    .max(100, 'O email deve ter no máximo 100 caractéres.'),
+  password: z.string().min(8, 'A senha deve conter ao menos 8 caractéres.'),
+})
+
+type LoginSchema = z.infer<typeof loginSchema>
+
+export function LoginForm() {
+  //   const router = useRouter()
+
+  const { register, handleSubmit, formState } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema),
+  })
+
+  function handleUserLogin(data: LoginSchema) {
+    console.log(data)
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(handleUserLogin)}
+      className='flex flex-col gap-3 text-left my-2'
+    >
+      <div>
+        <Label htmlFor='email' className='font-bold md:text-lg'>
+          Email
+        </Label>
+        <Input
+          autoFocus
+          id='email'
+          type='email'
+          placeholder='exemplo@exemplo.com'
+          className='md:text-lg md:h-12'
+          {...register('email')}
+        />
+        {formState.errors.email && (
+          <p className='text-destructive'>{formState.errors.email.message}</p>
+        )}
+      </div>
+      <div>
+        <Label htmlFor='senha' className='font-bold md:text-lg'>
+          Senha
+        </Label>
+        <Input
+          id='email'
+          type='password'
+          placeholder='********'
+          className='md:text-lg md:h-12'
+          {...register('password')}
+        />
+        {formState.errors.password && (
+          <p className='text-destructive'>
+            {formState.errors.password.message}
+          </p>
+        )}
+      </div>
+
+      <Button
+        size={'lg'}
+        type='submit'
+        className='bg-primary hover:bg-primary/95 md:text-lg'
+      >
+        Entrar
+      </Button>
+    </form>
+  )
+}
--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -1,250 +1,184 @@
 'use client'
-import { Input } from '@/components/ui/input'
 import { useState, type FormEvent } from 'react'
+import { LoginForm } from './_components/login-form'
 
 export default function clientePage() {
-  const [jaCadastrado, setJaCadastrado] = useState<boolean>(false)
-
-  const [nome, setNome] = useState('')
-  const [cpf, setCpf] = useState('')
-  const [email, setEmail] = useState('')
-  const [senha, setSenha] = useState('')
-  const [marca, setMarca] = useState('')
-  const [modelo, setModelo] = useState('')
-  const [ano, setAno] = useState('')
-  const [placa, setPlaca] = useState('')
-
-  function opcaoLogin(e: FormEvent) {
-    e.preventDefault()
-    setJaCadastrado(!jaCadastrado)
-  }
-
-  function acaoSubmitForm(e: FormEvent) {
-    e.preventDefault()
-
-    // Validação dos campos
-    if (!jaCadastrado) {
-      if (!nome.trim() || !cpf.trim() || !email.trim() || !senha.trim() || !marca.trim() || modelo.trim() || ano.trim() || placa.trim()) {
-        alert('Preencha todos os campos!')
-      }
-    } else if (!email.trim() || !senha.trim()) {
-      alert('Preencha todos os campos!')
-      return
-    }
-
-    if (senha.trim().length < 8) {
-      alert('A senha deve ter pelo menos 8 caracteres!')
-      return
-    }
-
-    const usuario = { nome: nome, email: email }
-
-    // Se todos os campos estiverem válidos
-    if (jaCadastrado) {
-      alert('Logado com sucesso!')
-      usuario.nome = 'Cliente' // Resgatar nome no banco
-    } else {
-      alert('Cadastrado com sucesso!')
-    }
-
-    localStorage.setItem('logado', JSON.stringify(usuario))
-    // nav('/area_assistente')
-
-    // Limpa os campos após a submissão
-    setNome('')
-    setCpf('')
-    setEmail('')
-    setSenha('')
-    setMarca('')
-    setModelo('')
-    setAno('')
-    setPlaca('')
-
-  }
+  const [cadastrado, setCadastrado] = useState<boolean>(false)
 
   return (
     <main className='w-full flex-grow flex flex-col'>
-      {/* conteudo cliente */}
       <div className='flex justify-around w-full min-h-[80vh] max-w-[1440px] mx-auto'>
-        <div className='py-6 px-5 my-auto text-center max-w-[431px] w-full '>
-          {localStorage.getItem('logado') ? (
-            <>
-              <h1 className='text-blue-600 font-bold text-4xl'>
-                Área do Cliente
-              </h1>
-              <button
-                type='button'
-                className='p-4 rounded-2xl mt-7 text-base font-semibold bg-blue-600 border border-blue-600 cursor-pointer hover:scale-98'
-                onClick={() => {
-                  //   nav('/area_assistente')
-                }}
-              >
-                Ir para a área do assistente
-              </button>
-              <button
-                type='button'
-                className='p-4 rounded-2xl mt-7 text-base font-semibold bg-blue-600 border border-blue-600 cursor-pointer hover:scale-98'
-                onClick={() => {
-                  localStorage.removeItem('logado')
-                  window.location.href = '/'
-                }}
-              >
-                Clique aqui para sair da conta
-              </button>
-            </>
-          ) : (
-            <>
-              <h1>{jaCadastrado ? 'Login' : 'Cadastro'}</h1>
-              <div className='mt-4 w-full flex flex-col gap-5'>
-                <legend className='w-4/5 mx-auto text-muted-foreground text-base mt-5'>
-                  {jaCadastrado
-                    ? 'Entre com seu email e senha para continuar'
-                    : 'Cadastre agora sua conta para continuar'}
-                </legend>
-                <form
-                  className='flex flex-col items-center max-w-lg w-full gap-5'
-                  onSubmit={acaoSubmitForm}>
-                  {jaCadastrado ? (
-                    ''
-                  ) : (
-                    <>
-                      <Input
-                        className='py-6 px-4'
-                        placeholder='Seu nome'
-                        type='text'
-                        id='nome'
-                        name='nome'
-                        value={nome} // TODO
-                        onChange={e => setNome(e.target.value)} // TODO
-                        required
-                      />
-                      <Input
-                        className='py-6 px-4'
-                        placeholder='Seu cpf'
-                        type='text'
-                        id='cpf'
-                        name='cpf'
-                        value={cpf} // TODO
-                        onChange={e => setCpf(e.target.value)} // TODO
-                        required
-                      />
-                    </>
-                  )}
-                  <Input
-                    className='py-6 px-4'
-                    placeholder='Seu email'
-                    type='text'
-                    id='email'
-                    name='email'
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                    required
-                  />
-                  <Input
-                    className='py-6 px-4'
-                    placeholder='Sua senha'
-                    type='password'
-                    id='senha'
-                    name='senha'
-                    value={senha}
-                    onChange={e => setSenha(e.target.value)}
-                    required
-                  />
-                  <a
-                    className='w-full mt-[2px] text-right text-sm no-underline text-blue-400 hover:text-blue-600'
-                    href='/client#'
-                  >
-                    Esqueceu a senha?
-                  </a>
+        {/* DIV FORM */}
+        <div className='py-6 px-5 my-auto text-center min-h-[500px] max-w-[431px] w-full '>
+          <h1 className='text-4xl font-extrabold'>
+            {cadastrado ? 'Login' : 'Cadastro'}
+          </h1>
+          <p className='mx-auto text-muted-foreground text-base mt-2'>
+            {cadastrado
+              ? 'Entre com seu email e senha para continuar'
+              : 'Cadastre agora sua conta para continuar'}
+          </p>
+          {cadastrado ? <LoginForm /> : ''}
+          {/* <RegisterForm /> */}
 
-                  <div className='botoes'>
-                    <button
-                      type='submit'
-                      onClick={acaoSubmitForm}
-                      className='bg-blue-600 no-underline border rounded-[15px] text-white font-semibold py-1 px-2 min-w-32 h-12 '
-                    >
-                      {jaCadastrado ? 'Login' : 'Cadastrar-se'}
-                    </button>
+          <p className='mx-auto flex justify-center text-left text-muted-foreground'>
+            {cadastrado ? 'Não tem uma conta?' : 'Já tem uma conta?'}
 
-                    {jaCadastrado ? (
-                      <p className='mx-auto flex text-left text-sm'>
-                        Não tem uma conta?
-                        <button type='button' onClick={opcaoLogin}>
-                          Criar uma conta aqui
-                        </button>
-                      </p>
-                    ) : (
-                      <p className='mx-auto flex text-left text-sm '>
-                        Já tem uma conta?
-                        <button
-                          className='text-left ml-1 font-semibold text-[0.95rem] bg-transparent border-none'
-                          type='button'
-                          onClick={opcaoLogin}
-                        >
-                          Login na conta aqui
-                        </button>
-                      </p>
-                    )}
-                  </div>
-                </form>
-              </div>
-            </>
-          )}
-          <div className='flex flex-col'>
-            <form className='flex flex-col items-center max-w-lg w-full gap-5' onSubmit={acaoSubmitForm}>
-              {jaCadastrado?(
+            <button
+              className='text-left ml-1 font-semibold text-[0.95rem] bg-transparent border-none text-foreground'
+              type='button'
+              onClick={() => setCadastrado(!cadastrado)}
+            >
+              {cadastrado ? 'Criar uma conta aqui' : 'Login na conta aqui'}
+            </button>
+          </p>
+
+          {/* <div className='mt-4 w-full flex flex-col gap-5'>
+            <form
+              className='flex flex-col items-center max-w-lg w-full gap-5'
+              onSubmit={acaoSubmitForm}
+            >
+              {jaCadastrado ? (
                 ''
-              ):(
+              ) : (
                 <>
-                <Input
-                className='py-6 px-4'
-                placeholder='Marca Carro'
-                type='text'
-                id='marca'
-                name='marca'
-                value = {marca}
-                onChange={e => setMarca (e.target.value)}
-                required
-                />
-                <Input
-                className='py-6 px-4'
-                placeholder='Modelo Carro'
-                type='text'
-                id='modelo'
-                name='modelo'
-                value = {modelo} 
-                onChange={e => setModelo (e.target.value)}
-                required
-                />
-                <Input
-                className='py-6 px-4'
-                placeholder='Ano Carro'
-                type='number'
-                min={1950}
-                max={2025}
-                id='ano'
-                name='ano'
-                value = {ano}
-                onChange={e => setAno (e.target.value)}
-                required
-                />
-                <Input
-                className='py-6 px-4'
-                placeholder='Placa Carro'
-                type='text'
-                id='placa'
-                name='placa'
-                value = {placa}
-                onChange={e => setPlaca (e.target.value)}
-                required
-                />
-                
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Seu nome'
+                    type='text'
+                    id='nome'
+                    name='nome'
+                    value={nome} // TODO
+                    onChange={e => setNome(e.target.value)} // TODO
+                    required
+                  />
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Seu cpf'
+                    type='text'
+                    id='cpf'
+                    name='cpf'
+                    value={cpf} // TODO
+                    onChange={e => setCpf(e.target.value)} // TODO
+                    required
+                  />
                 </>
-              )
-              }
+              )}
+              <Input
+                className='py-6 px-4'
+                placeholder='Seu email'
+                type='text'
+                id='email'
+                name='email'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+              <Input
+                className='py-6 px-4'
+                placeholder='Sua senha'
+                type='password'
+                id='senha'
+                name='senha'
+                value={senha}
+                onChange={e => setSenha(e.target.value)}
+                required
+              />
+              <a
+                className='w-full mt-[2px] text-right text-sm no-underline text-blue-400 hover:text-blue-600'
+                href='/client#'
+              >
+                Esqueceu a senha?
+              </a>
+
+              <div className='botoes'>
+                <button
+                  type='submit'
+                  onClick={acaoSubmitForm}
+                  className='bg-blue-600 no-underline border rounded-[15px] text-white font-semibold py-1 px-2 min-w-32 h-12 '
+                >
+                  {jaCadastrado ? 'Login' : 'Cadastrar-se'}
+                </button>
+
+                {jaCadastrado ? (
+                  <p className='mx-auto flex text-left text-sm'>
+                    Não tem uma conta?
+                    <button type='button' onClick={opcaoLogin}>
+                      Criar uma conta aqui
+                    </button>
+                  </p>
+                ) : (
+                  <p className='mx-auto flex text-left text-sm '>
+                    Já tem uma conta?
+                    <button
+                      className='text-left ml-1 font-semibold text-[0.95rem] bg-transparent border-none'
+                      type='button'
+                      onClick={opcaoLogin}
+                    >
+                      Login na conta aqui
+                    </button>
+                  </p>
+                )}
+              </div>
             </form>
           </div>
+          <div className='flex flex-col'>
+            <form
+              className='flex flex-col items-center max-w-lg w-full gap-5'
+              onSubmit={acaoSubmitForm}
+            >
+              {jaCadastrado ? (
+                ''
+              ) : (
+                <>
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Marca Carro'
+                    type='text'
+                    id='marca'
+                    name='marca'
+                    value={marca}
+                    onChange={e => setMarca(e.target.value)}
+                    required
+                  />
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Modelo Carro'
+                    type='text'
+                    id='modelo'
+                    name='modelo'
+                    value={modelo}
+                    onChange={e => setModelo(e.target.value)}
+                    required
+                  />
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Ano Carro'
+                    type='number'
+                    min={1950}
+                    max={2025}
+                    id='ano'
+                    name='ano'
+                    value={ano}
+                    onChange={e => setAno(e.target.value)}
+                    required
+                  />
+                  <Input
+                    className='py-6 px-4'
+                    placeholder='Placa Carro'
+                    type='text'
+                    id='placa'
+                    name='placa'
+                    value={placa}
+                    onChange={e => setPlaca(e.target.value)}
+                    required
+                  />
+                </>
+              )}
+            </form>
+          </div> */}
         </div>
+        {/* IMAGEM CELULAR */}
         <div className='imagem_celular_home'>
           <img src='image/imagem_celular_login.png' alt='Imagem com celular' />
         </div>
