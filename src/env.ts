2e518472e1c27df3ff7326e1c4c88f421d3fbb1f
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2024-10-21 20:57:20 -0300

    feat: auth setup

---
+++ src/env.ts
@@ -0,0 +1,19 @@
+import z from 'zod'
+
+const envSchema = z.object({
+  JWT_SECRET: z.string(),
+  COOKIE_SECRET: z.string(),
+  BACKEND_URL: z.string().url(),
+  AI_URL: z.string().url(),
+  AI_KEY: z.string(),
+})
+
+const envVars = {
+  JWT_SECRET: process.env.JWT_SECRET,
+  COOKIE_SECRET: process.env.COOKIE_SECRET,
+  BACKEND_URL: process.env.BACKEND_URL,
+  AI_URL: process.env.AI_URL,
+  AI_KEY: process.env.AI_KEY,
+}
+
+export const clientEnv = envSchema.parse(envVars)
